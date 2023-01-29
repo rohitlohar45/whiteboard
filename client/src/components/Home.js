669ccd292790828182d6cc429bb2a8ab23fba01f
Rohit Lohar <loharrohit9860@gmail.com>
2023-01-29 13:01:30 +0530

    Endpoint

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -30,7 +30,7 @@ export class Home extends Component {
 
 	render() {
 		const { authenticated, token, isLoading, name, board } = this.props;
-		if (isLoading && token) {
+		if (authenticated === null && isLoading && token) {
 			return (<div className='flexbox uc cu fullvh fullWidth'><Loading msg='Authenticating'></Loading></div>)
 		} else if (!authenticated) {
 			return (
@@ -42,7 +42,32 @@ export class Home extends Component {
 					</div >
 				</div>
 			)
-		}  else {
+		} else if (authenticated === true) {
+			return (
+				<div>
+					<CreateBoardModal doRender={this.state.createBoardModal} toggle={this.toggleCreateBoard.bind(this)} authenticated={authenticated}></CreateBoardModal>
+					<div className='flexbox fullvh'>
+						<LeftBar toggleCreateBoard={this.toggleCreateBoard.bind(this)}></LeftBar>
+
+						<div className='p2 fullWidth' style={{ minWidth: 0, height: '100vh' }}>
+							<h2>Hello {name}</h2>
+							<Spacer height='2rem'></Spacer>
+
+							<h2 style={{ height: '50px' }}>{board.isLoading ? <Loading></Loading> : 'Your DrawBoards'}</h2>
+							<BoardsList isLoading={board.isLoading && board.boards && board.boards.length === 0} list={board.boards}></BoardsList>
+							{
+								board.isLoading === false && board.boards.length === 0 ? <span>You do not have any boards ;(</span> : null
+							}
+							{/* 
+                        <Spacer></Spacer>
+
+                        <h2>Bookmarked DrawBoards</h2>
+                        <BoardsList list={''}></BoardsList> */}
+						</div>
+					</div>
+				</div>
+			)
+		} else {
 			return (<div></div>)
 		}
 	}
