c27c42f20233acd73430b1a794eee35f4acd6089
Rohit Lohar <loharrohit9860@gmail.com>
2023-01-29 12:19:53 +0530

    Endpoint

--- client/src/actions/boardActions.js
+++ client/src/actions/boardActions.js
@@ -1,6 +1,8 @@
 import { GET_ONE_BOARD_ERROR, GET_ONE_BOARD, ONE_BOARD_LOADING, GET_BOARDS, ADD_BOARD, BOARDS_LOADING, CLEAR_ERRORS, CLEAR_BOARDS } from "./types";
 
 export const getBoards = () => async (dispatch, getState) => {
+    const URL = 'https://whiteboard-cwbp.onrender.com/';
+    
     dispatch(setBoardsLoading());
 
     const res = await fetch('/api/user/boards', {
@@ -63,7 +65,7 @@ export const addBoard = board => async (dispatch, getState) => {
             body: JSON.stringify(board)
         });
         const data = await res.json();
-        'https://whiteboard-cwbp.onrender.com/board/' + data.board._id;
+        URL += 'board/' + data.board._id;
     }
 };
 
